a045d475b0131be76c6fb2050026e99cda530820
Modurotolu Olokode <durolawk@gmail.com>
2020-11-10 17:31:47 +0100

    added functionality to handle empty requests

--- addresses/addresses-router.js
+++ addresses/addresses-router.js
@@ -4,15 +4,16 @@ const data = require('./mock-addresses');
 router.post('/', (req, res) => {
   try {
     let address = req.body.search;
-    console.log(address);
     let result = [];
     
-    for (let i = 0; data.length > i; i++) {
-      if (data[i].toLowerCase().includes(address.toLowerCase()) && result.length < 5) {
-        result.push(data[i]);
-      };
-      if (result.length >= 4) {
-        break
+    if (address) {
+      for (let i = 0; data.length > i; i++) {
+        if (data[i].toLowerCase().includes(address.toLowerCase()) && result.length < 5) {
+          result.push(data[i]);
+        };
+        if (result.length >= 4) {
+          break
+        };
       };
     };
     res.status(201).json(result);
